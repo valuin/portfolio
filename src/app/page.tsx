2f0067e08928fe0d621ed97fe90ab676af20122c
Valtrizt Khalifah Warri <valvaltrizt@gmail.com>
2024-08-07 03:32:44 +0700

    feat: About Me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import React from "react";
+import Footer from "@/components/Footer";
+import About from "@/components/About";
 import { ShaderGradientCanvas, ShaderGradient } from "shadergradient";
 import * as reactSpring from "@react-spring/three";
 import * as drei from "@react-three/drei";
@@ -7,62 +9,54 @@ import * as fiber from "@react-three/fiber";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div
-        className="text-container"
-        style={{ position: "relative", zIndex: 2 }}
-      >
-        <h1 className="text-4xl font-bold text-center mb-4">
-          Valtrizt Test Site
-        </h1>
-      </div>
-      <ShaderGradientCanvas
-        importedFiber={{ ...fiber, ...drei, ...reactSpring }}
-        style={{
-          position: "absolute",
-          pointerEvents: "none",
-          top: 0,
-          left: 0,
-          zIndex: 1, // Ensure this is lower than the text container's z-index
-        }}
-      >
-        <ShaderGradient
-          control="query"
-          urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&brightness=4&cAzimuthAngle=180&cDistance=2.8&cPolarAngle=80&cameraZoom=9.1&color1=%23606080&color2=%238d7dca&color3=%23212121&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=on&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.1&rotationX=50&rotationY=0&rotationZ=-60&shader=defaults&type=waterPlane&uAmplitude=0&uDensity=1.5&uFrequency=0&uSpeed=0.3&uStrength=1.5&uTime=8&wireframe=false&zoomOut=false"
-        />
-      </ShaderGradientCanvas>
-      <footer
-        className="w-full py-4 mt-8 bg-neutral-900 rounded-xl text-white text-center"
-        style={{
-          backgroundColor: "rgba(255, 255, 255, 0.1)",
-          backdropFilter: "blur(10px)",
-          WebkitBackdropFilter: "blur(10px)", // For Safari compatibility
-          border: "1px solid rgba(255, 255, 255, 0.18)",
-          boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-          position: "relative",
-          zIndex: 10,
-        }}
-      >
-        <p>
-          &copy; {new Date().getFullYear()} Made by Valtrizt
-        </p>
-        <div className="flex justify-center space-x-4 mt-2">
-          <a
-            href="https://twitter.com/yourhandle"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            LinkedIn
-          </a>
-          <a
-            href="https://instagram.com/yourhandle"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Instagram
-          </a>
+    <main className="flex flex-col min-h-screen bg-customDark overflow-hidden">
+      <div className="relative w-full h-screen">
+        <ShaderGradientCanvas
+          importedFiber={{ ...fiber, ...drei, ...reactSpring }}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            pointerEvents: "none",
+          }}
+        >
+          <ShaderGradient
+            control="query"
+            urlString={process.env.NEXT_PUBLIC_SHADER_GRADIENT_URL}
+          />
+        </ShaderGradientCanvas>
+
+        <div className="absolute inset-0 flex flex-col justify-center items-start p-8 z-10">
+          <h1 className="text-white text-5xl md:text-7xl lg:text-9xl font-bold mb-4 text-shadow-glow">
+            Valtrizt
+          </h1>
+          <h2 className="text-white text-2xl md:text-3xl lg:text-5xl font-medium ml-1 text-shadow-glow">
+            Fullstack Dev
+          </h2>
         </div>
-      </footer>
+
+        <div className="absolute bottom-0 left-0 w-full h-2/3 bg-gradient-to-b from-transparent to-customDark pointer-events-none"></div>
+      </div>
+
+      <div className="relative w-full min-h-screen bg-customDark mt-8">
+        <div
+          className="absolute inset-0 bg-cover bg-center"
+          style={{
+            backgroundImage: `url('/2nd Section.png')`,
+          }}
+        ></div>
+        <About />
+      </div>
+      <div className="relative w-full min-h-screen bg-customDark mt-0">
+        <div
+          className="absolute inset-0 bg-cover bg-center"
+          style={{
+            backgroundImage: `url('/ExpF.png')`,
+          }}
+        ></div>
+      </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { PluginAPI } from "tailwindcss/types/config";
 
 const config: Config = {
   content: [
@@ -8,13 +9,34 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        customDark: "#06000E",
+        customRed: "#C52344",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      textShadow: {
+        "3xl": "0 25px 50px -12px #000000",
+        "4xl": "0 30px 60px -15px #C52344",
+        'glow': '0 0 10px rgba(255, 255, 255, 0.5), 0 0 20px rgba(255, 255, 255, 0.4), 0 0 30px rgba(255, 255, 255, 0.3), 0 0 40px rgba(255, 255, 255, 0.2), 0 0 50px rgba(255, 255, 255, 0.1)',
+        'redglow': '0 0 10px rgba(197, 35, 68, 0.5), 0 0 20px rgba(197, 35, 68, 0.4), 0 0 30px rgba(197, 35, 68, 0.3), 0 0 40px rgba(197, 35, 68, 0.2), 0 0 50px rgba(197, 35, 68, 0.1)',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities } : PluginAPI) {
+      addUtilities({
+        '.text-shadow-glow': {
+          textShadow: '0 0 10px rgba(255, 255, 255, 0.5), 0 0 20px rgba(255, 255, 255, 0.4), 0 0 30px rgba(255, 255, 255, 0.3), 0 0 40px rgba(255, 255, 255, 0.2), 0 0 50px rgba(255, 255, 255, 0.1)',
+        },
+        '.text-shadow-redglow': {
+          textShadow: '0 0 10px rgba(197, 35, 68, 0.5), 0 0 20px rgba(197, 35, 68, 0.4), 0 0 30px rgba(197, 35, 68, 0.3), 0 0 40px rgba(197, 35, 68, 0.2), 0 0 50px rgba(197, 35, 68, 0.1)',
+        },
+      });
+    },
+  ],
 };
 export default config;
