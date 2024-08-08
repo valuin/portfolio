e415c4aace23609068d7be83c64a8d91294cc57a
Valtrizt Khalifah Warri <valvaltrizt@gmail.com>
2024-08-08 18:10:09 +0700

    feat: better bento

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 import Image from "next/image";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogImage,
+  DialogSubtitle,
+  DialogClose,
+  DialogDescription,
+  DialogContainer,
+} from "@/components/core/dialog";
 
 interface Item {
   image?: string;
@@ -39,42 +50,55 @@ const BentoGrid: React.FC<BentoGridProps> = ({ items }) => (
 );
 
 const Projects: React.FC = () => {
-  const projectItems = [
+  const projectItems: Item[] = [
     {
       title: "Project 1",
       description: "Description 1",
-      image: "/Magdalene.png",
-      className: "col-span-2 row-span-2",
-    },
-    {
-      title: "Project 2",
-      description: "Description 2",
-      image: "/Calculator.png",
-      className: "col-span-1 row-span-2",
-    },
-    {
-      title: "Project 3",
-      description: "Description 3",
-      image: "/Magdalene.png",
+      image: "/Econity.png",
       className: "col-span-2 row-span-1",
-    },
-    {
-      title: "Project 4",
-      description: "Description 4",
-      image: "/Calculator.png",
-      className: "col-span-3 row-span-2",
-    },
-    {
-      title: "Project 5",
-      description: "Description 5",
-      image: "/Magdalene.png",
-      className: "col-span-1 row-span-2",
+      aspectRatio: "2 / 1",
     },
     {
       title: "Project 6",
       description: "Description 6",
-      image: "/Magdalene.png",
+      image: "/Salon.png",
       className: "col-span-2 row-span-1",
+      aspectRatio: "2 / 1",
+    },
+    {
+      title: "Project 3",
+      description: "Description 3",
+      image: "/Code.jpg",
+      className: "col-span-1 row-span-1",
+      aspectRatio: "1 / 1",
+    },
+    {
+      title: "Project 5",
+      description: "Description 5",
+      image: "/Calculator.png",
+      className: "col-span-1 row-span-3",
+      aspectRatio: "9 / 16",
+    },
+    {
+      title: "Project 7",
+      description: "Description 7",
+      image: "/Mupi.png",
+      className: "col-span-2 row-span-2",
+      aspectRatio: "16 / 9",
+    },
+    {
+      title: "Project 3",
+      description: "Description 3",
+      image: "/Dashboard.png",
+      className: "col-span-1 row-span-1",
+      aspectRatio: "9 / 16",
+    },
+    {
+      title: "Project 2",
+      description: "Description 2",
+      image: "/Moneasy.png",
+      className: "col-span-2 row-span-1",
+      aspectRatio: "16 / 9",
     },
   ];
 
